d3c2426d7b1fcedf5deb776d5e346540b7be20c1
DamianJimenoRodriguez <104791234+DamianJimenoRodriguez@users.noreply.github.com>
2022-10-27 18:18:59 +0200

    AnimatioUPdate

---
+++ Repo/Assets/scripts/AnimationTest.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTest : MonoBehaviour
+{
+	public Animator myAnimator;
+
+	private void Update()
+	{
+	}
+}
