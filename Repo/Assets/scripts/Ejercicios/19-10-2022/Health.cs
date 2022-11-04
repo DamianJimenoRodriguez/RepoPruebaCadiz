ba53bd9e6a0c22983ada1fbb46bfa82348013f3c
DamianJimenoRodriguez <104791234+DamianJimenoRodriguez@users.noreply.github.com>
2022-11-04 19:46:41 +0100

    Enemy spawner

--- Repo/Assets/scripts/Ejercicios/19-10-2022/Health.cs
+++ Repo/Assets/scripts/Ejercicios/19-10-2022/Health.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class PlayerHealth : MonoBehaviour
+public class Health : MonoBehaviour
 {
 	public int MaxHealth;
 	private int currentHealth;
@@ -17,24 +17,35 @@ public class PlayerHealth : MonoBehaviour
 	{
 		InitialPosition = transform.position;
 		currentHealth = MaxHealth;
-		healthText.text = currentHealth.ToString();
+
+		if (healthText != null)
+		{
+			healthText.text = currentHealth.ToString();
+		}
 	}
 
 	public void TakeDamage(int damageTaken)
 	{
 		currentHealth -= damageTaken;
-		healthText.text = currentHealth.ToString();
+
+		if (healthText != null)
+		{
+			healthText.text = currentHealth.ToString();
+		}
+
 		if (currentHealth <= 0)
 		{
-			Debug.Log("estoy muerto");
-			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-			//transform.position = InitialPosition;
-			//currentHealth = MaxHealth;
-			//healthText.text = currentHealth.ToString();
+			//Debug.Log("estoy muerto");
+			//SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+			Die();
 		}
 		else
 		{
 			Debug.Log("me han echo daño, salud restante " + currentHealth);
 		}
+	}
+
+	public virtual void Die()
+	{
 	}
 }
