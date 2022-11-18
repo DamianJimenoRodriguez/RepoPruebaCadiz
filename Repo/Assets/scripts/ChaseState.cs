a78f06e00445d7139823c7506e7e31736070b731
DamianJimenoRodriguez <104791234+DamianJimenoRodriguez@users.noreply.github.com>
2022-11-18 19:20:45 +0100

    State machines

---
+++ Repo/Assets/scripts/ChaseState.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseState : State
+{
+	public float chaseSpeed = 3.5f;
+	public float AttackDistance;
+	public float IdleDistance;
+
+	public State idleState;
+	public State attactkState;
+
+	public override State RunCurrentState()
+	{
+		if (Vector3.Distance(transform.position, player.position) > IdleDistance)
+		{
+			return idleState;
+		}
+		else if (Vector3.Distance(transform.position, player.position) < AttackDistance)
+		{
+			return attactkState;
+		}
+		else
+		{
+			agent.destination = player.position;
+			agent.speed = chaseSpeed;
+			return this;
+		}
+	}
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, AttackDistance);
+	}
+}
